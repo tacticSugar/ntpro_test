c895137c3532edbf0a9d3ce4d429d158611dbcd9
Vladimir <balaian.vladimir@yandex.ru>
2022-12-05 21:43:30 +0300

    fix size_v1

--- src/components/Archive/Archive.tsx
+++ src/components/Archive/Archive.tsx
@@ -8,32 +8,30 @@ type ArchiveProps = {
 
 export default function Archive({ archiveList }: ArchiveProps) {
   return (
-    <div className="table_container">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Side</th>
-            <th>Price</th>
-            <th>Instrument</th>
-            <th>Volume</th>
-            <th>Timestamp</th>
-          </tr>
-        </thead>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Side</th>
+          <th>Price</th>
+          <th>Instrument</th>
+          <th>Volume</th>
+          <th>Timestamp</th>
+        </tr>
+      </thead>
 
-        <tbody>
-          {[...archiveList].reverse().map((el) => (
-            <tr key={el.time.toISOString()}>
-              <td className={el.side === 'buy' ? 'green' : 'red'}>
-                {el.side.toString().toUpperCase()}
-              </td>
-              <td>{el.price}</td>
-              <td>{el.instrument}</td>
-              <td>{el.volume}</td>
-              <td>{dayjs(el.time).format('YYYY.MM.DD H:mm:s.sss')}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <tbody>
+        {[...archiveList].reverse().map((el) => (
+          <tr key={el.time.toISOString()}>
+            <td className={el.side === 'buy' ? 'green' : 'red'}>
+              {el.side.toString().toUpperCase()}
+            </td>
+            <td>{el.price}</td>
+            <td>{el.instrument}</td>
+            <td>{el.volume}</td>
+            <td>{dayjs(el.time).format('YYYY.MM.DD H:mm:s.sss')}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
